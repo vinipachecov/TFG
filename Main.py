d32762e18c849fe40a9e1d391d4276cd48afa491
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2017-09-08 19:21:24 -0300

    Encoding categorical data

--- Main.py
+++ Main.py
@@ -5,4 +5,27 @@ Spyder Editor
 Este é um arquivo de script temporário.
 """
 
-import pandas
+import pandas as pd
+import numpy as np
+
+# Criando setup
+
+dataframe = pd.read_csv('/home/vinicius/Repos/TFG/data.csv')
+X = dataframe.iloc[:, 2:-1].values
+
+# 
+Y = dataframe.iloc[:, 1].values
+Y = np.matrix(dataframe.iloc[:, 1].values)
+
+
+
+# Encoding categorical data
+#Maligo = 1  | Benigno = 0
+for i in range(Y.shape[1]):
+    if Y[0,i] == 'M':
+        Y[0,i] = 1
+    else:
+        Y[0,i] = 0
+        
+        
+    
